cf1a82a512ca845d2ca9b9979604c6c966fc6207
Corentin <corentin.charriot@epitech.eu>
2018-12-06 18:27:51 +0100

    localhost

--- index.js
+++ index.js
@@ -11,9 +11,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', function (req, res) {
-
 });
 
-app.listen(5000, "51.75.91.104", function (req, res) {
-    console.log("Server launched on 51.75.91.104:5000");
+app.listen(5000, function (req, res) {
+    console.log("Server launched on port 5000");
 });
